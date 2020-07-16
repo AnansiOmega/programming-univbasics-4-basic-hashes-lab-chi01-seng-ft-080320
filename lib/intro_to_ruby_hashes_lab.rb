e638ac5c803188429c7d0796b4458fe53a9a4afb
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-16 21:49:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,14 @@ def new_hash
 end
 
 def my_hash
- { favorite_color: "green"
+ { favorite_color: "green" }
 end
 
 def pioneer
-  
+{ name: "Grace Hopper" }
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  { id: number }
 end
